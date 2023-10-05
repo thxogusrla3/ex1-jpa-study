2552f403558547797f75d60de9e7d3614f38be4c
김태현 <thxogusrla@gmail.com>
2023-10-05 23:22:17 +0900

    1005 영속성 컨텍스트

--- read.md
+++ read.md
@@ -16,3 +16,41 @@
 > JPA를 사용하면 엔티티 객체를 중심으로 개발되기 때문에 모든 DB 데이터를 객체로 변환해서 검색하는 것은 불가능 한 이유로 생김.
 - 필요한 데이터만 DB에서 불러오려면 결국 검색 조건이 포함된 SQL이 필요하고 JPQL 이 이를 지원함.
 - JPQL은 ***엔티티 객체***, SQL은 ***데이터베이스 테이블*** 을 대상으로 쿼리
+
+## JPA에서 가장 중요한 2가지
+- 객체와 관계형 데이터베이스 매핑하기
+- 영속성 컨텍스트
+
+## 영속성 컨텍스트란
+- 엔티티를 영구 저장하는 환경
+
+### 1차 캐시
+- 영속성 컨텍스트 내부에 있는 첫 번째 캐시,
+- 조회 시 1차 캐시에 데이터가 이미 있는지 확인하고, 데이터가 있으면 가져온다.
+- 1차 캐시에 데이터가 없다면, 데이터베이스에 데이터를 요청한다.
+- 데이터베이스에서 받은 데이터를 다음에 재사용할 수 있도록 1차 캐시에 저장한다.
+```
+Member a = em.find(Member.class, 105L);
+Member a1 = em.find(Member.class, 105L);
+Member b = em.find(Member.class, 101L);
+Member b1 = em.find(Member.class, 101L);
+
+System.out.println("a.name = " + a.getName());
+System.out.println("b.name = " + b.getName());
+System.out.println("a1.name = " + a1.getName());
+System.out.println("b1.name = " + b1.getName());
+
+//a1, b1 은 앞서 실행된 a, b가 1차 캐시에 남아서 조회쿼리는 두 번만 실행된다.
+```
+
+### 변경 감지(Dirty Checking)
+- commit() 후에 flush()가 실행되고, 1차 캐시에는 ID, Entity, SnapShot 이 생김
+- 그 후 엔티티와 스냅샷을 비교하고 쓰기지연SQL 저장소에 UPDATE 쿼리를 날림
+- 즉 객체를 변경하게 되면 자동으로 UPDATE 쿼리가 실행된다고 생각하면 됨.
+```
+Member updateMember = em.find(Member.class, 105);
+updateMember.setName("test");
+commit();
+```
+### flush() 란?
+- 
--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -1,7 +1,67 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class jpaMain {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            /* 영속성 컨텍스트 예시 */
+            //객체를 생성한 상태(비영속) (commit 주석 풀것)*/
+            Member member = new Member();
+            member.setId(106L);
+            member.setName("ㅇㅇㅇ");
+            //객체를 생성한 상태(비영속)
+
+            //객체를 영속성 컨텍스트에 저장한 상태(영속)
+//            em.persist(member);
+            //객체를 영속성 컨텍스트에 저장한 상태(영속)
+            /* 영속성 컨텍스트 예시 */
+            tx.commit();
+            
+            /* 1차 캐시 예제 */
+            // find 는 총 4번이지만, select 쿼리는 1차 캐시로 인해 2개만 생성된다.
+            Member a = em.find(Member.class, 105L);
+            Member a1 = em.find(Member.class, 105L);
+            Member b = em.find(Member.class, 101L);
+            Member b1 = em.find(Member.class, 101L);
+
+            System.out.println("a.name = " + a.getName());
+            System.out.println("b.name = " + b.getName());
+            System.out.println("a1.name = " + a1.getName());
+            System.out.println("b1.name = " + b1.getName());
+            /* 1차 캐시 예제 */
+            
+            /* 쓰기 지연 예시(commit 주석 풀것) */
+            //영속성 컨텍스트에 저장해도 DB에 바로 insert 쿼리를 날리지 않는다.
+            tx.begin();
+            Member memberA = new Member(200L, "memberA");
+            Member memberB = new Member(205L, "memberB");
+
+            em.persist(memberA);
+            em.persist(memberB);
+
+//            tx.commit();
+            /* 쓰기 지연 예시 */
+            
+            /* 변경 감지 예시 */
+            Member updateMember = em.find(Member.class, 105);
+            System.out.println("before " + updateMember.getName());
+            updateMember.setName("test");
+            System.out.println("after " + updateMember.getName());
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            //무조건 닫아줘야함.
+            em.clear();
+        }
     }
 }
